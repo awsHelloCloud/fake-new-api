1aa2535cd38a546daad7b840ddbb06c13eda3e49
m24927605 <m24927605@gmail.com>
2019-11-14 21:13:50 +0800

    integration test done

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const express = require('express');
 const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
-
+const rp = require('request-promise');
 
 const app = express();
 const neo4j = require('neo4j-driver').v1;
@@ -14,20 +14,21 @@ const driver = neo4j.driver(
   {database:'fakenews'}
 );
 
-const session = driver.session();
-const id1='AWEBRBJ8yCdS-nWhunDm'.replace('-','');
-const id2='AWEMi_sOhutQxxU6tp-E'.replace('-','');
-const text1=`木耳發泡超8小時，變質後的細菌量將暴增數倍至幾十倍！而浙江陳先生吃的木耳泡了72小時之多，細菌量更是百倍地暴增！`;
-const text2=`公告：嘉市環保局便民服務，每日晚上十點半至凌晨三點，會有一台垃圾車，定點在嘉女高中對面，提拱服務。全年無休，只休除夕一天，`
-const userId1='user_1';
-const userId2='user_2';
-const roomId1='room_1';
-const roomId2='room_2';
-const roomId3='room_3';
-const roomId4='room_4';
 (async()=>{
+  const id1='AWEBRBJ8yCdS-nWhunDm'.replace('-','');
+  const id2='AWEMi_sOhutQxxU6tp-E'.replace('-','');
+  const text1=`木耳發泡超8小時，變質後的細菌量將暴增數倍至幾十倍！而浙江陳先生吃的木耳泡了72小時之多，細菌量更是百倍地暴增！`;
+  const text2=`公告：嘉市環保局便民服務，每日晚上十點半至凌晨三點，會有一台垃圾車，定點在嘉女高中對面，提拱服務。全年無休，只休除夕一天，`
+  const userId1='user_1';
+  const userId2='user_2';
+  const roomId1='room_1';
+  const roomId2='room_2';
+  const roomId3='room_3';
+  const roomId4='room_4';
+  const session = driver.session();
+  await session.run(`MATCH (n) DETACH DELETE n`);
+/*
   try {
-    await session.run(`MATCH (n) DETACH DELETE n`);
 
     await session.run(`
     CREATE 
@@ -69,7 +70,7 @@ const roomId4='room_4';
     
   } catch (e) {
    console.error(e) 
-  }
+  }*/
   session.close();
   driver.close();
 })()
@@ -88,9 +89,89 @@ app.get('/', (req,res,next)=>{
   res.sned('hello world')
 });
 
-app.post('/valid-fakenews', (req,res,next)=>{
-  res.json({valid_result:true})
-});
+app.use('/valid-fakenews',(req,res,next)=>{
+  const {roomId,userId,text}=req.body;
+
+  if(!roomId || !userId|| !text){
+    res.status=400;
+    res.json({err_message:'INVALID_PARAMETER'});
+  }else{
+    next();
+  }
+})
+
+app.post('/valid-fakenews',async (req,res,next)=>{
+    try {
+      const session = driver.session();
+    console.log('[req.body]',req.body);
+    const {roomId,userId,text}=req.body;
+    const options = {
+      method: 'POST',
+      uri: 'https://aad0647b.ngrok.io/isFakeNews',
+      formData: {
+        text: text
+      },
+      json: true // Automatically stringifies the body to JSON
+    };
+    console.log('[options]',options)
+    const response=await rp(options);
+    console.log('[response]',response)
+    if(response.articleId===null && response.fakeNews===false){
+      res.json({
+        is_fake:'NO'
+      })
+    }else if(typeof response.articleId ==='string' && response.fakeNews===true){
+      const id='`'+response.articleId+'`';
+
+      const rumorResult= await session.run(`
+      MATCH (${id}:Rumor {id:'${id}',text:'${text}'})
+      RETURN ${id}
+      `)
+      if(rumorResult.records.length===0){
+        await session.run(`
+        CREATE 
+        (${id}:Rumor {id:'${id}',text:'${text}'})
+        `);
+      }
+
+      const userSpreadRumorInRoomResult= await session.run(`
+      MATCH (${id}:Rumor {id:'${id}',text:'${text}'})
+      <-[:SPREAD]-(r:Room {roomId:'${roomId}'})
+      RETURN r.roomId
+      `)
+      if(userSpreadRumorInRoomResult.records.length===0){
+        await session.run(`
+        MATCH (${id}:Rumor {id:'${id}',text:'${text}'})
+        CREATE 
+        (${userId}:LineUser {userId:'${userId}',name:'${new Date().getTime()}'})
+        -[:TALK_IN]->
+        (${roomId}:Room {roomId:'${roomId}',roomName:'${new Date().getTime()}'})
+        -[:SPREAD]->
+        (${id})
+        `);
+        res.json({is_fake:true});  
+      }else{
+        await session.run(`
+        MATCH (${id}:Rumor {id:'${id}',text:'${text}'})
+        CREATE 
+        (${userId}:LineUser {userId:'${userId}',name:'${new Date().getTime()}'})
+        -[:TALK_IN]->
+        (${roomId}:Room {roomId:'${roomId}',roomName:'${new Date().getTime()}'})
+        -[:SPREAD]->
+        (${id})
+        `);
+        res.json({is_fake:'YES'}); 
+      }
+      session.close();
+    }else{
+      res.json({is_fake:'UNKNOWN'})
+    }
+  } catch (e) {
+    console.error('[error]',e.message)
+   res.status=500;
+   res.json({'err_message':e.message}) 
+  }
+  });
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
