4a92827cba1fd4da32417744eacb9874540f3a63
m24927605 <m24927605@gmail.com>
2019-11-16 09:46:01 +0800

    use dotenv

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+require('dotenv').config()
 const createError = require('http-errors');
 const express = require('express');
 const path = require('path');
@@ -10,67 +11,12 @@ const neo4j = require('neo4j-driver').v1;
 
 const driver = neo4j.driver(
   'bolt://localhost',
-  neo4j.auth.basic('neo4j', 'fakenews'),
-  {database:'fakenews'}
+  neo4j.auth.basic('neo4j', 'fakenews')
 );
 
 (async()=>{
-  const id1='AWEBRBJ8yCdS-nWhunDm'.replace('-','');
-  const id2='AWEMi_sOhutQxxU6tp-E'.replace('-','');
-  const text1=`木耳發泡超8小時，變質後的細菌量將暴增數倍至幾十倍！而浙江陳先生吃的木耳泡了72小時之多，細菌量更是百倍地暴增！`;
-  const text2=`公告：嘉市環保局便民服務，每日晚上十點半至凌晨三點，會有一台垃圾車，定點在嘉女高中對面，提拱服務。全年無休，只休除夕一天，`
-  const userId1='user_1';
-  const userId2='user_2';
-  const roomId1='room_1';
-  const roomId2='room_2';
-  const roomId3='room_3';
-  const roomId4='room_4';
   const session = driver.session();
   await session.run(`MATCH (n) DETACH DELETE n`);
-/*
-  try {
-
-    await session.run(`
-    CREATE 
-    (${id1}:Rumor {id:'${id1}',text:'${text1}'}),
-    (${id2}:Rumor {id:'${id2}',text:'${text2}'}),
-
-    (${userId1}:LineUser {userId:'${userId1}'})
-    -[:TALK_IN]->
-    (${roomId1}:Room {roomId:'${roomId1}'})
-    -[:SPREAD]->
-    (${id1}),
-    (${userId1})
-    -[:TALK_IN]->
-    (${roomId3}:Room {roomId:'${roomId3}'})
-    -[:SPREAD]->
-    (${id2}),
-
-    (${userId2}:LineUser {userId:'${userId2}'})
-    -[:TALK_IN]->
-    (${roomId2}:Room {roomId:'${roomId2}'})
-    -[:SPREAD]->
-    (${id1}),
-    (${userId2})
-    -[:TALK_IN]->
-    (${roomId4}:Room {roomId:'${roomId4}'})
-    -[:SPREAD]->
-    (${id1})
-    `);
-    const result= await session.run(`
-    MATCH (AWEMi_sOhutQxxU6tpE {id:'AWEMi_sOhutQxxU6tpE'})
-    <-[:SPREAD]-(r:Room)
-    <-[:TALK_IN]-(u:LineUser) 
-    RETURN u.userId
-    `)
-    
-    result.records.forEach((record)=>{
-      console.log(record);
-    })
-    
-  } catch (e) {
-   console.error(e) 
-  }*/
   session.close();
   driver.close();
 })()
@@ -100,6 +46,13 @@ app.use('/valid-fakenews',(req,res,next)=>{
   }
 })
 
+app.post('/isFakeNews',(req,res,next)=>{
+  res.json({
+    articleId:'AWEMi_sOhutQxxU6tp-E',
+    fakeNews:true
+  })
+})
+
 app.post('/valid-fakenews',async (req,res,next)=>{
     try {
       const session = driver.session();
@@ -107,7 +60,7 @@ app.post('/valid-fakenews',async (req,res,next)=>{
     const {roomId,userId,text}=req.body;
     const options = {
       method: 'POST',
-      uri: 'https://aad0647b.ngrok.io/isFakeNews',
+      uri: `${FAKENEWS_DATA_SERVER_HOST}:${FAKENEWS_DATA_SERVER_PORT}/isFakeNews`,
       formData: {
         text: text
       },
@@ -145,23 +98,23 @@ app.post('/valid-fakenews',async (req,res,next)=>{
         CREATE 
         (${userId}:LineUser {userId:'${userId}',name:'${new Date().getTime()}'})
         -[:TALK_IN]->
-        (${roomId}:Room {roomId:'${roomId}',roomName:'${new Date().getTime()}'})
+        (${roomId}:Room {roomId:'${roomId}'})
         -[:SPREAD]->
         (${id})
         `);
-        res.json({is_fake:true});  
       }else{
         await session.run(`
         MATCH (${id}:Rumor {id:'${id}',text:'${text}'})
+        MATCH (${roomId}:Room {roomId:'${roomId}'})
         CREATE 
         (${userId}:LineUser {userId:'${userId}',name:'${new Date().getTime()}'})
         -[:TALK_IN]->
-        (${roomId}:Room {roomId:'${roomId}',roomName:'${new Date().getTime()}'})
+        (${roomId})
         -[:SPREAD]->
         (${id})
         `);
-        res.json({is_fake:'YES'}); 
       }
+      res.json({is_fake:'YES'}); 
       session.close();
     }else{
       res.json({is_fake:'UNKNOWN'})
