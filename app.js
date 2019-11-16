1794f834420cffc2d833d04a2f146d48c40dc822
m24927605 <m24927605@gmail.com>
2019-11-16 16:09:18 +0800

    tracking done

--- app.js
+++ app.js
@@ -27,7 +27,7 @@ const driver = neo4j.driver(
 
 (async()=>{
   const session = driver.session();
-  await session.run(`MATCH (n) DETACH DELETE n`);
+  //await session.run(`MATCH (n) DETACH DELETE n`);
   session.close();
   driver.close();
 })()
@@ -59,82 +59,97 @@ app.use('/valid-fakenews',(req,res,next)=>{
 
 app.post('/isFakeNews',(req,res,next)=>{
   res.json({
-    articleId:'AWEMi_sOhutQxxU6tp-E',
+    articleId:'AWEMi_sOhutQxxU6tp-E'+`${new Date().getTime()}`,
     fakeNews:true
   })
 })
 
+const findRumorSource=async(id)=>{
+  const session = driver.session();
+  const userList= await session.run(`
+  MATCH (${id}:Rumor)
+  <-[:SPREAD]-(r:Room)
+  <-[:TALK_IN]-
+  (user:LineUser)
+  RETURN r,user
+  `)
+  return userList;
+}
+
 app.post('/valid-fakenews',async (req,res,next)=>{
     try {
       const session = driver.session();
-    console.log('[req.body]',req.body);
-    const {roomId,userId,text}=req.body;
-    const options = {
-      method: 'POST',
-      uri: `${FAKENEWS_DATA_SERVER_HOST}:${FAKENEWS_DATA_SERVER_PORT}/isFakeNews`,
-      formData: {
-        text: text
-      },
-      json: true // Automatically stringifies the body to JSON
-    };
-    console.log('[options]',options)
-    const response=await rp(options);
-    console.log('[response]',response)
-    if(response.articleId===null && response.fakeNews===false){
-      res.json({
-        is_fake:'NO'
-      })
-    }else if(typeof response.articleId ==='string' && response.fakeNews===true){
-      const id='`'+response.articleId+'`';
+      console.log('[req.body]',req.body);
+      const {roomId,userId,text}=req.body;
+      const options = {
+        method: 'POST',
+        uri: `${FAKENEWS_DATA_SERVER_HOST}:${FAKENEWS_DATA_SERVER_PORT}/isFakeNews`,
+        formData: {
+          text: text
+        },
+        json: true // Automatically stringifies the body to JSON
+      };
+      console.log('[options]',options)
+      const response=await rp(options);
+      console.log('[response]',response)
+      if(response.articleId===null && response.fakeNews===false){
+        res.json({
+          is_fake:'NO'
+        })
+      }else if(typeof response.articleId ==='string' && response.fakeNews===true){
+        const id='`'+response.articleId+'`';
 
-      const rumorResult= await session.run(`
-      MATCH (${id}:Rumor {id:'${id}',text:'${text}'})
-      RETURN ${id}
-      `)
-      if(rumorResult.records.length===0){
-        await session.run(`
-        CREATE 
-        (${id}:Rumor {id:'${id}',text:'${text}'})
-        `);
-      }
-
-      const userSpreadRumorInRoomResult= await session.run(`
-      MATCH (${id}:Rumor {id:'${id}',text:'${text}'})
-      <-[:SPREAD]-(r:Room {roomId:'${roomId}'})
-      RETURN r.roomId
-      `)
-      if(userSpreadRumorInRoomResult.records.length===0){
-        await session.run(`
+        const rumorResult= await session.run(`
         MATCH (${id}:Rumor {id:'${id}',text:'${text}'})
-        CREATE 
-        (${userId}:LineUser {userId:'${userId}',name:'${new Date().getTime()}'})
-        -[:TALK_IN]->
-        (${roomId}:Room {roomId:'${roomId}'})
-        -[:SPREAD]->
-        (${id})
-        `);
+        RETURN ${id}
+        `)
+        if(rumorResult.records.length===0){
+          await session.run(`
+          CREATE 
+          (${id}:Rumor {id:'${id}',text:'${text}'})
+          `);
+        }
+
+        const userSpreadRumorInRoomResult= await session.run(`
+        MATCH (${id}:Rumor {id:'${id}',text:'${text}'})
+        <-[:SPREAD]-(r:Room {roomId:'${roomId}'})
+        RETURN r.roomId
+        `)
+        if(userSpreadRumorInRoomResult.records.length===0){
+          await session.run(`
+          MATCH (${id}:Rumor {id:'${id}',text:'${text}'})
+          CREATE 
+          (${userId}:LineUser {userId:'${userId}',name:'${new Date().getTime()}'})
+          -[:TALK_IN]->
+          (${roomId}:Room {roomId:'${roomId}'})
+          -[:SPREAD]->
+          (${id})
+          `);
+        }else{
+          await session.run(`
+          MATCH (${id}:Rumor {id:'${id}',text:'${text}'})
+          MATCH (${roomId}:Room {roomId:'${roomId}'})
+          CREATE 
+          (${userId}:LineUser {userId:'${userId}',name:'${new Date().getTime()}'})
+          -[:TALK_IN]->
+          (${roomId})
+          -[:SPREAD]->
+          (${id})
+          `);
+        }
+         const userList=await findRumorSource(id);
+         const total=userList.records.length;
+         const source=userList.records[0]._fields[1].properties.userId;
+        res.json({is_fake:'YES',total,source}); 
+        session.close();
       }else{
-        await session.run(`
-        MATCH (${id}:Rumor {id:'${id}',text:'${text}'})
-        MATCH (${roomId}:Room {roomId:'${roomId}'})
-        CREATE 
-        (${userId}:LineUser {userId:'${userId}',name:'${new Date().getTime()}'})
-        -[:TALK_IN]->
-        (${roomId})
-        -[:SPREAD]->
-        (${id})
-        `);
+        res.json({is_fake:'UNKNOWN'})
       }
-      res.json({is_fake:'YES'}); 
-      session.close();
-    }else{
-      res.json({is_fake:'UNKNOWN'})
+    } catch (e) {
+      console.error('[error]',e.message)
+    res.status=500;
+    res.json({'err_message':e.message}) 
     }
-  } catch (e) {
-    console.error('[error]',e.message)
-   res.status=500;
-   res.json({'err_message':e.message}) 
-  }
   });
 
 // catch 404 and forward to error handler
